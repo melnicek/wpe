511ff3df0a40d94f8c301e87f195a7ba9a37151f
melnicek <melnicek@pm.me>
2020-09-09 11:40:12 +0200

    Update

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/Privesc/PowerUp.ps1
+wget https://raw.githubusercontent.com/rasta-mouse/Sherlock/master/Sherlock.ps1
+wget https://raw.githubusercontent.com/411Hall/JAWS/master/jaws-enum.ps1
+wget https://raw.githubusercontent.com/AonCyberLabs/Windows-Exploit-Suggester/master/windows-exploit-suggester.py
+echo "Nice, now you can serve your files using `sudo ./serve.py`"
